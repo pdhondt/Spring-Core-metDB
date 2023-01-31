c4f5e074131748f9980a30a0018672bb0d01e6da
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-31 21:19:52 +0100

    hoofdstuk 16 & 17 - Repository Test & SQL Statements Loggen

---
+++ src/test/java/be/vdab/geld/repositories/MensRepositoryTest.java
@@ -0,0 +1,95 @@
+package be.vdab.geld.repositories;
+
+import be.vdab.geld.domain.Mens;
+import be.vdab.geld.exceptions.MensNietGevondenException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@JdbcTest
+@Import(MensRepository.class)
+@Sql("/mensen.sql")
+public class MensRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
+    private static final String MENSEN = "mensen";
+    private final MensRepository mensRepository;
+    public MensRepositoryTest(MensRepository mensRepository) {
+        this.mensRepository = mensRepository;
+    }
+    @Test
+    void findAantal() {
+        assertThat(mensRepository.findAantal())
+                .isEqualTo(countRowsInTable(MENSEN));
+    }
+    @Test
+    void findAllGeeftAlleMensenGesorteerdOpId() {
+        assertThat(mensRepository.findAll())
+                .hasSize(countRowsInTable(MENSEN))
+                .extracting(Mens::getId)
+                .isSorted();
+    }
+    @Test
+    void create() {
+        var id = mensRepository.create(new Mens(0, "test3", BigDecimal.TEN));
+        assertThat(id).isPositive();
+        assertThat(countRowsInTableWhere(MENSEN, "id = " + id)).isOne();
+    }
+    private long idVanTestMens1() {
+        return jdbcTemplate.queryForObject(
+                "select id from mensen where naam = 'test1'", Long.class);
+    }
+    @Test
+    void delete() {
+        var id = idVanTestMens1();
+        mensRepository.delete(id);
+        assertThat(countRowsInTableWhere(MENSEN, "id = " + id)).isZero();
+    }
+    @Test
+    void findById() {
+        assertThat(mensRepository.findById(idVanTestMens1())).hasValueSatisfying(
+                mens -> assertThat(mens.getNaam()).isEqualTo("test1"));
+    }
+    @Test
+    void findByOnbestaandeIdVindtGeenMens() {
+        assertThat(mensRepository.findById(Long.MAX_VALUE)).isEmpty();
+    }
+    @Test
+    void update() {
+        var id = idVanTestMens1();
+        var mens = new Mens(id, "mens1", BigDecimal.TEN);
+        mensRepository.update(mens);
+        assertThat(countRowsInTableWhere(MENSEN, "geld = 10 and id = " + id)).isOne();
+    }
+    @Test
+    void updateOnbestaandeMensGeeftEenFout() {
+        assertThatExceptionOfType(MensNietGevondenException.class).isThrownBy(
+                () -> mensRepository.update(
+                        new Mens(Long.MAX_VALUE, "test3", BigDecimal.TEN)));
+    }
+    @Test
+    void findAndLockById() {
+        assertThat(mensRepository.findAndLockById(idVanTestMens1()))
+                .hasValueSatisfying(
+                        mens -> assertThat(mens.getNaam()).isEqualTo("test1"));
+    }
+    @Test
+    void findAndLockByOnbestaandeIdVindtGeenMens() {
+        assertThat(mensRepository.findAndLockById(Long.MAX_VALUE)).isEmpty();
+    }
+    @Test
+    void findByGeldBetween() {
+        var van = BigDecimal.ONE;
+        var tot = BigDecimal.TEN;
+        assertThat(mensRepository.findByGeldBetween(van, tot))
+                .hasSize(super.countRowsInTableWhere(MENSEN, "geld between 1 and 10"))
+                .extracting(Mens::getGeld)
+                .allSatisfy(geld -> assertThat(geld).isBetween(van, tot))
+                .isSorted();
+    }
+}
---
+++ src/test/resources/mensen.sql
@@ -0,0 +1,2 @@
+insert into mensen(naam, geld)
+values ('test1', 1000), ('test2', 2000);
